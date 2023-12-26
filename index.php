40921acb6072d9be5e60f98b8e3df4ee1e4722d5
Miron Pontak <pontak.miron@gmail.com>
2023-12-26 16:53:05 +0700

    Getting the request method and its data
    added functions for obtaining the request method and its data, such as body, parameters

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+function getMethod() {
+    return $_SERVER['REQUEST_METHOD'];
+}
+
+
+function getData($method) {
+    $data = new stdClass();
+    $data->body = json_decode(file_get_contents('php://input'));
+
+    $data->parameters = [];
+    $dataGet = $_GET;
+    foreach ($dataGet as $key => $value) {
+        if ($key != "q") {
+            $data->parameters[$key] = $value;
+        }
+    }
+    return $data;
+}
+
+
 header('Content-type: application/json');
 $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
 
