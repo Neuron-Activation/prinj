adeba8ddb5264b303eb13b663e989feef6202240
Miron Pontak <pontak.miron@gmail.com>
2023-12-26 19:30:17 +0700

    Authorization data validation
    added email validation, added display of appropriate error if the data doesn't pass validation

--- routers/login.php
+++ routers/login.php
@@ -1,12 +1,20 @@
 <?php
 
 include_once 'utils/token.php';
+include_once 'validators/emailValidator.php';
 
 
 function route($method, $urlList, $requestData) {
     if ($method == "POST") {
         $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
         $email = $requestData->body->email;
+
+        if (!validateEmail($email)) {
+            http_response_code(400);
+            echo "Invalid email format.";
+            return;
+        }
+
         $password = hash("sha1", $requestData->body->password);
         $user = $link->query("SELECT id from users where email = '$email' AND password = '$password'")->fetch_assoc();
 
