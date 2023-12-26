8b2c3fdd28fbc2380c595155ef2539565ac26d21
Miron Pontak <pontak.miron@gmail.com>
2023-12-26 19:25:51 +0700

    Authorization endpoint
    added an authorization endpoint, added receiving data from the request body, generating a request to the database for authorization, generating token, added a check for the existence of the user

---
+++ routers/login.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once 'utils/token.php';
+
+
+function route($method, $urlList, $requestData) {
+    if ($method == "POST") {
+        $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
+        $email = $requestData->body->email;
+        $password = hash("sha1", $requestData->body->password);
+        $user = $link->query("SELECT id from users where email = '$email' AND password = '$password'")->fetch_assoc();
+
+        if (!is_null($user)) {
+            $token = generateToken($link, $user);
+            echo json_encode(array("token" => $token));
+        }
+    }
+}
+
+?>
