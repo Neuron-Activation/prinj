72638499227c2df3c885d31e42ce8946c4af64ab
Miron Pontak <pontak.miron@gmail.com>
2023-12-26 19:34:36 +0700

    Authorization error handling
    added error output if the request method is incorrect or the URL is incorrect, added an error output if a user with the same email and password doesn't exist in the system

--- routers/login.php
+++ routers/login.php
@@ -5,24 +5,44 @@ include_once 'validators/emailValidator.php';
 
 
 function route($method, $urlList, $requestData) {
-    if ($method == "POST") {
-        $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
-        $email = $requestData->body->email;
+    if ($method !== "POST") {
+        http_response_code(405);
+        echo "Method Not Allowed";
+        return;
+    }
 
-        if (!validateEmail($email)) {
-            http_response_code(400);
-            echo "Invalid email format.";
-            return;
-        }
+    if (count($urlList) > 1) {
+        http_response_code(404);
+        echo "Page Not Found";
+        return;
+    }
+    
+    $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
 
-        $password = hash("sha1", $requestData->body->password);
-        $user = $link->query("SELECT id from users where email = '$email' AND password = '$password'")->fetch_assoc();
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+        echo "Failed to connect to database: " . mysqli_connect_error();
+        return;
+    }
 
-        if (!is_null($user)) {
+    $email = $requestData->body->email;
+
+    if (!validateEmail($email)) {
+        http_response_code(400);
+        echo "Invalid email format.";
+        return;
+    }
+
+    $password = hash("sha1", $requestData->body->password);
+    $user = $link->query("SELECT id from users where email = '$email' AND password = '$password'")->fetch_assoc();
+
+    if (!is_null($user)) {
             $token = generateToken($link, $user);
             echo json_encode(array("token" => $token));
+        } else {
+            http_response_code(401);
+            echo "Unauthorized: User with this email and password not found.";
         }
-    }
 }
 
 ?>
