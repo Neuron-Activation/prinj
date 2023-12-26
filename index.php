78616b6f6497f7f75200e26116ca7352803a780d
Miron Pontak <pontak.miron@gmail.com>
2023-12-26 16:50:49 +0700

    Сonnection to database
    added connection to the database and checking for connection errors

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
+header('Content-type: application/json');
+$link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
+
+if (!$link) {
+    echo "Ошибка: невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    exit;
+}
+
 ?>
