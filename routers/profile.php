9508eacabf3f403d3207ea79171a564980277426
Miron Pontak <pontak.miron@gmail.com>
2023-12-27 20:43:50 +0700

    Profile endpoint error handling
    added error output if the request method is incorrect or the URL is incorrect, added error output if any of the request body fields are empty, added error output if the database request was not completed

--- routers/profile.php
+++ routers/profile.php
@@ -4,11 +4,25 @@ include_once 'validators/emailValidator.php';
 include_once 'validators/phoneValidator.php';
 include_once 'validators/genderValidator.php';
 
+include_once 'utils/token.php';
+
 
 function route($method, $urlList, $requestData) {
     $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
 
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+        echo "Failed to connect to database: " . mysqli_connect_error();
+        return;
+    }
+
     $userId = checkToken();
+
+    if (is_null($userId)) {
+        http_response_code(401);
+        echo "Unauthorized: Access denied.";
+        return;
+    }
 
     switch ($method) {
         case 'GET':
@@ -24,6 +38,12 @@ function route($method, $urlList, $requestData) {
             $phone = $requestData->body->phone;
             $email = $requestData->body->email;
 
+            if (empty($name) || empty($dateOfBirth) || empty($gender) || empty($phone) || empty($email)) {
+                http_response_code(400);
+                echo "All fields are required.";
+                return;
+            }
+
             if (!validateEmail($email)) {
                 http_response_code(400);
                 echo "Invalid email format.";
@@ -35,14 +55,22 @@ function route($method, $urlList, $requestData) {
                 echo "Invalid phone number format. Use +7 (xxx) xxx-xx-xx.";
                 return;
             }
-        
+
             if (!validateGender($gender)) {
                 http_response_code(400);
                 echo "Gender can only be male or female.";
                 return;
             }
 
-            $link->query("UPDATE users SET name = '$name', date_of_birth = '$dateOfBirth', gender = '$gender', phone = '$phone', email = '$email' WHERE id = '$userId'");
+            $userUpdateResult = $link->query("UPDATE users SET name = '$name', date_of_birth = '$dateOfBirth', gender = '$gender', phone = '$phone', email = '$email' WHERE id = '$userId'");
+
+            if (!$userUpdateResult) {
+                http_response_code(500);
+                echo "Internal Server Error";
+                return;
+            } else {
+                echo "User information has been successfully updated";
+            }
 
             break;
         default:
