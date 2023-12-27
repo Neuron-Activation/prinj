1e752aa131ef836a57cccf62d7040180144a45a5
Miron Pontak <pontak.miron@gmail.com>
2023-12-27 20:31:25 +0700

    Validation of new profile data
    added functions for validating email, phone number, gender used when editing user's information, added display of appropriate errors if the data doesn't pass validation

--- routers/profile.php
+++ routers/profile.php
@@ -1,5 +1,10 @@
 <?php
 
+include_once 'validators/emailValidator.php';
+include_once 'validators/phoneValidator.php';
+include_once 'validators/genderValidator.php';
+
+
 function route($method, $urlList, $requestData) {
     $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
 
@@ -18,6 +23,24 @@ function route($method, $urlList, $requestData) {
             $gender = $requestData->body->gender;
             $phone = $requestData->body->phone;
             $email = $requestData->body->email;
+
+            if (!validateEmail($email)) {
+                http_response_code(400);
+                echo "Invalid email format.";
+                return;
+            }
+
+            if (!validatePhone($phone)) {
+                http_response_code(400);
+                echo "Invalid phone number format. Use +7 (xxx) xxx-xx-xx.";
+                return;
+            }
+        
+            if (!validateGender($gender)) {
+                http_response_code(400);
+                echo "Gender can only be male or female.";
+                return;
+            }
 
             $link->query("UPDATE users SET name = '$name', date_of_birth = '$dateOfBirth', gender = '$gender', phone = '$phone', email = '$email' WHERE id = '$userId'");
 
