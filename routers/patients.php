2e027819aca6f77c170cc0691411cd8bd3f38657
Miron Pontak <pontak.miron@gmail.com>
2023-12-28 16:37:37 +0700

    Sort patients by name and creation date
    added code for sorting patients by name and creation date, both in ascending and descending order

--- routers/patients.php
+++ routers/patients.php
@@ -5,8 +5,26 @@ function route($method, $urlList, $requestData) {
 
     $page = $_GET['page'] ?? 1;
     $pageSize = $_GET['pageSize'] ?? 5;
+    $sorting = $_GET['sorting'] ?? 'NameAsc';
 
-    $sql = "SELECT * FROM patients LIMIT " . (($page - 1) * $pageSize) . ", " . $pageSize;
+    switch ($sorting) {
+        case 'NameAsc':
+            $orderBy = "name ASC";
+            break;
+        case 'NameDesc':
+            $orderBy = "name DESC";
+            break;
+        case 'CreateAsc':
+            $orderBy = "create_time ASC";
+            break;
+        case 'CreateDesc':
+            $orderBy = "create_time DESC";
+            break;
+        default:
+            $orderBy = "name ASC";
+    }
+
+    $sql = "SELECT * FROM patients ORDER BY $orderBy LIMIT " . (($page - 1) * $pageSize) . ", " . $pageSize;
 
     $result = mysqli_query($link, $sql);
     $patients = mysqli_fetch_all($result, MYSQLI_ASSOC);
