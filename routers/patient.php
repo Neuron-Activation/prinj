5fa64e7292030111973323ff2a36e774cf704639
Miron Pontak <pontak.miron@gmail.com>
2023-12-29 19:11:24 +0700

    Patient inspection list
    added code to get a list of inspections for a specific user, added pagination, sorting by date, filtering by diagnosis codes, by chains of interconnected inspections

--- routers/patient.php
+++ routers/patient.php
@@ -15,6 +15,7 @@ function createPatient($name, $dateOfBirth, $gender) {
     }
 }
 
+
 function getPatientCard($patientId) {
     global $link;
 
@@ -23,9 +24,65 @@ function getPatientCard($patientId) {
 }
 
 
+function getPatientInspections($patientId, $page, $pageSize, $sorting, $icdRoots, $grouped) {
+    global $link;
+
+    switch ($sorting) {
+        case 'DateAsc':
+            $orderBy = "date ASC";
+            break;
+        case 'DateDesc':
+            $orderBy = "date DESC";
+            break;
+        default:
+            $orderBy = "date ASC";
+    }
+
+    $sql = "SELECT inspections.* FROM inspections WHERE patient_id = " . intval($patientId);
+
+    if ($grouped) {
+        $sql .= " AND previous_inspection_id IS NOT NULL";
+    }
+
+    $sql .= " ORDER BY $orderBy";
+
+    $result = mysqli_query($link, $sql);
+    $inspections = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    foreach ($inspections as $key => &$inspection) {
+        $diagnosisSql = "SELECT diagnoses.* FROM inspections_diagnoses LEFT JOIN diagnoses ON inspections_diagnoses.diagnosis_code = diagnoses.diagnosis_code WHERE inspection_id = " . $inspection['id'];
+
+        $diagnosisResult = mysqli_query($link, $diagnosisSql);
+        $inspection['diagnoses'] = mysqli_fetch_all($diagnosisResult, MYSQLI_ASSOC);
+
+        if ($icdRoots) {
+            $icdRootsArray = explode(',', $icdRoots);
+            $diagnosisCodes = array_column($inspection['diagnoses'], 'diagnosis_code');
+
+            if (array_diff($icdRootsArray, $diagnosisCodes) || array_diff($diagnosisCodes, $icdRootsArray)) {
+                unset($inspections[$key]);
+                continue;
+            }
+        }
+    }
+
+    $inspections = array_slice($inspections, ($page - 1) * $pageSize, $pageSize);
+
+    $response = [
+        'inspections' => array_values($inspections),
+        'pagination' => [
+            'size' => $pageSize,
+            'count' => count($inspections),
+            'current' => $page
+        ]
+    ];
+
+    echo json_encode($response);
+}
+
 
 function route($method, $urlList, $requestData) {
-   
+
     $userId = checkToken();
 
     if ($method === 'POST') {
@@ -35,6 +92,17 @@ function route($method, $urlList, $requestData) {
 
     if ($method === 'GET' && count($urlList) === 2) {
         getPatientCard($urlList[1]);
+        return;
+    }
+
+    if ($method === 'GET' && count($urlList) === 3) {
+        $page = $_GET['page'] ?? 1;
+        $pageSize = $_GET['pageSize'] ?? 5;
+        $sorting = $_GET['sorting'] ?? 'DateDesc';
+        $icdRoots = isset($_GET['icdRoots']) ? $_GET['icdRoots'] : null;
+        $grouped = isset($_GET['grouped']) ? $_GET['grouped'] : false;
+
+        getPatientInspections($urlList[1], $page, $pageSize, $sorting, $icdRoots, $grouped);
         return;
     }
 }
