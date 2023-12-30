86715709e0fbeef641e499d0ea59e5ee91e18d8d
Miron Pontak <pontak.miron@gmail.com>
2023-12-30 20:17:52 +0700

    Getting a chain of inspections
    added code for generating and sending queries to the database to get a chain of inspections by root inspection id

--- routers/inspection.php
+++ routers/inspection.php
@@ -2,63 +2,139 @@
 
 $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
 
+if (mysqli_connect_errno()) {
+    http_response_code(500);
+    echo "Failed to connect to database: " . mysqli_connect_error();
+    return;
+}
+
 include_once 'utils/token.php';
 
 
-function createInspection($date, $anamnesis, $complaints, $treatment, $conclusion, $nextVisitDate, $deathDate, $baseInspectionId, $previousInspectionId, $patientId, $consultationId, $diagnoses, $doctorId) {
+function createPatient($name, $dateOfBirth, $gender) {
     global $link;
-    $sql = "INSERT INTO inspections(date, anamnesis, complaints, treatment, conclusion, next_visit_date, death_date, base_inspection_id, previous_inspection_id, patient_id, consultation_id, doctor_id, create_time) VALUES('$date', '$anamnesis', '$complaints', '$treatment', '$conclusion', '$nextVisitDate', '$deathDate', '$baseInspectionId', '$previousInspectionId', '$patientId', '$consultationId', '$doctorId', CURRENT_TIMESTAMP)";
-    $link->query($sql);
-    
-    $inspection_id = $link->insert_id;
 
-    if (is_string($diagnoses)) {
-        $diagnoses = array($diagnoses);
-    }
-
-    foreach ($diagnoses as $code) {
-        $sql = "INSERT INTO inspections_diagnoses(inspection_id, diagnosis_code) VALUES('$inspection_id', '$code')";
-
-        if (!$link->query($sql)) {
-            http_response_code(500);
-            echo "Internal Server Error";
-            return;
-        }
+    if (!$link->query("INSERT INTO patients(name, date_of_birth, gender, create_time) VALUES('$name', '$dateOfBirth', '$gender', CURRENT_TIMESTAMP)")) {
+        http_response_code(500);
+        echo "Internal Server Error";
+        return;
+    } else {
+        echo "Patient Succesfully created.";
     }
 }
 
 
-function getInspection($inspectionId) {
+function getPatientCard($patientId) {
     global $link;
 
-    $result = $link->query("SELECT * from inspections where id = '$inspectionId'");
+    $patientId = $link->real_escape_string($patientId);
+    $result = $link->query("SELECT * from patients where id = '$patientId'");
 
-    $inspection = $result->fetch_assoc();
+    if ($link->error) {
+        http_response_code(500);
+        echo "Internal Server Error";
+        return;
+    }
 
-    if (!$inspection) {
+    $patient = $result->fetch_assoc();
+
+    if (!$patient) {
         http_response_code(404);
         echo "Patient not found";
         return;
     }
 
-    echo json_encode($inspection);
+    echo json_encode($patient);
 }
 
 
-function updateInspection($anamnesis, $complaints, $treatment, $conclusion, $nextVisitDate, $deathDate, $diagnoses, $inspectionId) {
+function getPatientInspections($patientId, $page, $pageSize, $sorting, $icdRoots, $grouped) {
     global $link;
 
-    $sql = "UPDATE inspections SET anamnesis = '$anamnesis', complaints = '$complaints', treatment = '$treatment', conclusion = '$conclusion', next_visit_date = '$nextVisitDate', death_date = '$deathDate' WHERE id = '$inspectionId'";
-
-    $sql = "DELETE FROM inspections_diagnoses WHERE inspection_id = '$inspectionId'";
-
-    if (is_string($diagnoses)) {
-        $diagnoses = array($diagnoses);
+    switch ($sorting) {
+        case 'DateAsc':
+            $orderBy = "date ASC";
+            break;
+        case 'DateDesc':
+            $orderBy = "date DESC";
+            break;
+        default:
+            $orderBy = "date ASC";
     }
 
-    foreach ($diagnoses as $code) {
-        $sql = "INSERT INTO inspections_diagnoses(inspection_id, diagnosis_code) VALUES('$inspectionId', '$code')";
+    $sql = "SELECT inspections.* FROM inspections WHERE patient_id = " . intval($patientId);
+
+    if ($grouped) {
+        $sql .= " AND previous_inspection_id IS NOT NULL";
     }
+
+    $sql .= " ORDER BY $orderBy";
+
+    $result = mysqli_query($link, $sql);
+
+    if (!$result) {
+        http_response_code(500);
+        echo "Internal Server Error";
+        return;
+    }
+
+    $inspections = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    foreach ($inspections as $key => &$inspection) {
+        $diagnosisSql = "SELECT diagnoses.* FROM inspections_diagnoses LEFT JOIN diagnoses ON inspections_diagnoses.diagnosis_code = diagnoses.diagnosis_code WHERE inspection_id = " . $inspection['id'];
+
+        $diagnosisResult = mysqli_query($link, $diagnosisSql);
+        $inspection['diagnoses'] = mysqli_fetch_all($diagnosisResult, MYSQLI_ASSOC);
+
+        if ($icdRoots) {
+            $icdRootsArray = explode(',', $icdRoots);
+            $diagnosisCodes = array_column($inspection['diagnoses'], 'diagnosis_code');
+
+            if (array_diff($icdRootsArray, $diagnosisCodes) || array_diff($diagnosisCodes, $icdRootsArray)) {
+                unset($inspections[$key]);
+                continue;
+            }
+        }
+    }
+
+    $inspections = array_slice($inspections, ($page - 1) * $pageSize, $pageSize);
+
+    $response = [
+        'inspections' => array_values($inspections),
+        'pagination' => [
+            'size' => $pageSize,
+            'count' => count($inspections),
+            'current' => $page
+        ]
+    ];
+
+    echo json_encode($response);
+}
+
+
+function getInspectionChain($baseInspectionId) {
+    global $link;
+    
+    $sql = "SELECT * FROM inspections WHERE base_inspection_id = " . intval($baseInspectionId) . " ORDER BY date DESC LIMIT 1";
+    $result = mysqli_query($link, $sql);
+    $latestInspection = mysqli_fetch_assoc($result);
+
+    $inspections = array($latestInspection);
+    $previousInspectionId = $latestInspection['previous_inspection_id'];
+    while ($previousInspectionId != null) {
+        $sql = "SELECT * FROM inspections WHERE id = " . intval($previousInspectionId);
+        $result = mysqli_query($link, $sql);
+        if (!$result) {
+            http_response_code(500);
+            echo "Internal Server Error";
+            return;
+        }
+        $previousInspection = mysqli_fetch_assoc($result);
+        array_push($inspections, $previousInspection);
+        $previousInspectionId = $previousInspection['previous_inspection_id'];
+    }
+
+    echo json_encode($inspections);
 }
 
 
@@ -66,39 +142,34 @@ function route($method, $urlList, $requestData) {
 
     $userId = checkToken();
 
-    if ($method === 'POST' && count($urlList) === 2 && $urlList[1] === "create") {
-        $date = $requestData->body->date;
-        $anamnesis = $requestData->body->anamnesis;
-        $complaints = $requestData->body->complaints;
-        $treatment = $requestData->body->treatment;
-        $conclusion = $requestData->body->conclusion;
-        $nextVisitDate = $requestData->body->nextVisitDate ?? null;
-        $deathDate = $requestData->body->deathDate ?? null;
-        $baseInspectionId = $requestData->body->baseInspectionId ?? null;
-        $previousInspectionId = $requestData->body->previousInspectionId ?? null;
-        $patientId = $requestData->body->patientId;
-        $consultationId = $requestData->body->consultationId;
-        $diagnoses = $requestData->body->diagnoses;
+    if (is_null($userId)) {
+        http_response_code(401);
+        echo "Unauthorized: Access denied.";
+        return;
+    }
 
-        createInspection($date, $anamnesis, $complaints, $treatment, $conclusion, $nextVisitDate, $deathDate, $baseInspectionId, $previousInspectionId, $patientId, $consultationId, $diagnoses, $userId);
+    if ($method !== "POST" && $method !== "GET") {
+        http_response_code(405);
+        echo "Method Not Allowed";
+        return;
+    }
+
+    if ($method === 'POST') {
+        createPatient($requestData->body->name, $requestData->body->date_of_birth, $requestData->body->gender);
         return;
     }
 
     if ($method === 'GET' && count($urlList) === 2) {
-        getInspection($urlList[1]);
+        getPatientCard($urlList[1]);
         return;
     }
 
-    if ($method === 'PUT' && count($urlList) === 2) {
-        $anamnesis = $requestData->body->anamnesis;
-        $complaints = $requestData->body->complaints;
-        $treatment = $requestData->body->treatment;
-        $conclusion = $requestData->body->conclusion;
-        $nextVisitDate = $requestData->body->nextVisitDate ?? null;
-        $deathDate = $requestData->body->deathDate ?? null;
-        $diagnoses = $requestData->body->diagnoses;
-
-        updateInspection($anamnesis, $complaints, $treatment, $conclusion, $nextVisitDate, $deathDate, $diagnoses, $urlList[1]);
+    if ($method === 'GET' && count($urlList) === 3 && $urlList[2] === "chain") {
+        getInspectionChain($urlList[1]);
+        return;
+    } else {
+        http_response_code(401);
+        echo "Page Not Found";
         return;
     }
 }
